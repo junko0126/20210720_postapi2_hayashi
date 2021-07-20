7f6d15f49cd1f08ef39c1d9811a196908a74dd66
junko0126 <hikari2011h.j@gmail.com>
2021-07-20 17:33:14 +0900

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,10 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+    <div class="home">
+      <input type="text" maxlength="7">
+      <button>住所自動入力</button>
+      <p>Address:</p>
     </div>
-    <router-view/>
   </div>
 </template>
 
